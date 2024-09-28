b5bba7b1075c360b0570b6c46c49e5c6c03ffaa5
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-09-28 10:33:47 +0200

    Initial commit

---
+++ src/main/java/com/lessons/java/spring/ticket/platform/TicketPlatfomrApplication.java
@@ -0,0 +1,13 @@
+package com.lessons.java.spring.ticket.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketPlatfomrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketPlatfomrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lessons/java/spring/ticket/platform/TicketPlatfomrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lessons.java.spring.ticket.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketPlatfomrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
