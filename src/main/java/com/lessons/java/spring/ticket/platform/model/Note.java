22d25c8f8c9aba0fd7a4c19612644c2874e24ed2
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-09-28 11:39:58 +0200

    add: create entity note

---
+++ src/main/java/com/lessons/java/spring/ticket/platform/model/Note.java
@@ -0,0 +1,70 @@
+package com.lessons.java.spring.ticket.platform.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+//entità notes
+@Entity
+@Table(name = "notes")
+public class Note {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@NotNull
+	@Size(min = 2, max = 255)
+	private String description;
+
+	private LocalDateTime createAt;
+
+	/**
+	 * relazione dove N note possono avere un solo ticket
+	 */
+	@ManyToOne
+	@JoinColumn(name = "ticket_id", nullable = false)
+	private Ticket ticket;
+
+	// getter - setter
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDateTime getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(LocalDateTime createAt) {
+		this.createAt = createAt;
+	}
+
+	public Ticket getTicket() {
+		return ticket;
+	}
+
+	public void setTicket(Ticket ticket) {
+		this.ticket = ticket;
+	}
+
+}
