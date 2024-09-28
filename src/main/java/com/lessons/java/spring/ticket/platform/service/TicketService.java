0f7f577e9cfd7294395786424813e66bc9af9961
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-09-28 14:16:11 +0200

    add: create method to return all tickets or all tickets with specific name

---
+++ src/main/java/com/lessons/java/spring/ticket/platform/service/TicketService.java
@@ -0,0 +1,38 @@
+package com.lessons.java.spring.ticket.platform.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lessons.java.spring.ticket.platform.model.Ticket;
+import com.lessons.java.spring.ticket.platform.repo.TicketRepository;
+
+@Service
+public class TicketService {
+
+	@Autowired
+	private TicketRepository repository;
+
+	/**
+	 * 
+	 * @return la lista dei tickets presenti nel repository
+	 */
+	public List<Ticket> findAllTickets() {
+
+		return repository.findAll();
+
+	}
+
+	/**
+	 * 
+	 * @param name, filto di ricerca del ticket;
+	 * @return la lista dei tickets che contentono il nome della ricerca;
+	 */
+	public List<Ticket> findAllByName(String name) {
+
+		return repository.findByName(name);
+
+	}
+
+}
