6cbb6aeb8dcc980a0ecd5c840760145c93e38d3f
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-09-28 14:16:38 +0200

    add: create query custom, method to find ticket by specific name

--- src/main/java/com/lessons/java/spring/ticket/platform/repo/TicketRepository.java
+++ src/main/java/com/lessons/java/spring/ticket/platform/repo/TicketRepository.java
@@ -1,9 +1,15 @@
 package com.lessons.java.spring.ticket.platform.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lessons.java.spring.ticket.platform.model.Ticket;
 
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
 
+	/**
+	 * query custom per cercare il ticket tramite nome
+	 */
+	public List<Ticket> findByName(String name);
 }
