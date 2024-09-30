a5702bc38fd9dbbf38f9b1ba33380fbdee60e5db
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-09-30 15:16:04 +0200

    add: create method update ticket

--- src/main/java/com/lessons/java/spring/ticket/platform/controller/TicketController.java
+++ src/main/java/com/lessons/java/spring/ticket/platform/controller/TicketController.java
@@ -5,13 +5,19 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.lessons.java.spring.ticket.platform.model.Ticket;
+import com.lessons.java.spring.ticket.platform.service.OperatorService;
 import com.lessons.java.spring.ticket.platform.service.TicketService;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/tickets")
@@ -19,6 +25,9 @@ public class TicketController {
 
 	@Autowired
 	private TicketService service;
+
+	@Autowired
+	private OperatorService serviceOperator;
 
 	/**
 	 * 
@@ -53,6 +62,49 @@ public class TicketController {
 		model.addAttribute("ticket", service.getById(id));
 
 		return "/tickets/single-ticket";
+
+	}
+
+	/**
+	 * Viene mostrato il form con i dati del ticket che si vuole modificare
+	 * 
+	 * @return i dati del ticket richiesto;
+	 * 
+	 */
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable("id") int id, Model model) {
+
+		model.addAttribute("ticket", service.getById(id));
+
+		String status = service.getById(id).getStatus();
+		service.getById(id).setStatus("");
+
+		model.addAttribute("status", status);
+
+		return "/tickets/form-edit-ticket";
+
+	}
+
+	/**
+	 * Memorizzazione della singola pizza modificata nel form
+	 * 
+	 * @return Se i dati sono sbagliati, viene riproposto il form da ricompilare
+	 * @return Una volta salvati i dati, viene restituita la lista dei ticket
+	 *         aggiornata
+	 */
+	@PostMapping("/edit/{id}")
+	public String update(@Valid @ModelAttribute("ticket") Ticket formTicket, BindingResult bindingResult, Model model) {
+
+		// Controllo se i campi compilati sono sbagliati
+		// Se ci sono errori ,viene restituita la pagina del form da ricompilare
+		// In caso contrario, i dati vengon salvati sul db
+		if (bindingResult.hasErrors()) {
+			return "/tickets/form-edit-ticket";
+		}
+
+		service.update(formTicket);
+
+		return "redirect:/tickets";
 
 	}
 
