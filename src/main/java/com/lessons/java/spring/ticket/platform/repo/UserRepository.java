51db1c4988b66082714f6837aed62b26c01339e1
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-10-05 09:08:45 +0200

    add: added repository to manage user from database

---
+++ src/main/java/com/lessons/java/spring/ticket/platform/repo/UserRepository.java
@@ -0,0 +1,13 @@
+package com.lessons.java.spring.ticket.platform.repo;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lessons.java.spring.ticket.platform.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+	public Optional<User> findByEmail(String email);
+
+}
