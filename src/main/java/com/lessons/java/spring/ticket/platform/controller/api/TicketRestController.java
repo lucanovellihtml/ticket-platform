91f6c581bfefac221e97125b927c1d594786dafc
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-10-05 11:55:07 +0200

    add: added api to return list tikets filtred by status

--- src/main/java/com/lessons/java/spring/ticket/platform/controller/api/TicketRestController.java
+++ src/main/java/com/lessons/java/spring/ticket/platform/controller/api/TicketRestController.java
@@ -6,14 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lessons.java.spring.ticket.platform.model.Ticket;
 import com.lessons.java.spring.ticket.platform.service.TicketService;
 
 /**
- * Controller per la gestione delle Rest Api sul'entity Ticket
- * CrossOrigin permette di richiamare l'API anche non da locale
+ * Controller per la gestione delle Rest Api sul'entity Ticket CrossOrigin
+ * permette di richiamare l'API anche non da locale
  */
 @RestController
 @CrossOrigin
@@ -22,9 +23,10 @@ public class TicketRestController {
 
 	@Autowired
 	private TicketService service;
-	
+
 	/**
 	 * Metodo che restituisce tutti i ticket presenti nel repository
+	 * 
 	 * @return la lista dei tickets presenti nel repository
 	 */
 	@GetMapping
@@ -37,5 +39,26 @@ public class TicketRestController {
 		return listTickets;
 
 	}
-	
+
+	/**
+	 * Metodo che restituisce tutti i ticket presenti nel repository con il filtro status
+	 * 
+	 * @return la lista dei tickets presenti nel repository con il filtro status
+	 */
+	@GetMapping("status-filter")
+	public List<Ticket> indexStatus(@RequestParam(name = "status", required = true) String status) {
+
+		List<Ticket> listTickets;
+		
+		if(status!= null && !status.isEmpty()) {
+			listTickets = service.findAllByStatus(status);
+		}
+		else {
+			listTickets = service.findAllTickets();
+		}
+
+		return listTickets;
+
+	}
+
 }
