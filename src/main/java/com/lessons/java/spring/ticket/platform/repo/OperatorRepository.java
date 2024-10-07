b2d91358b99d4070cce9e24621b6cf97311079df
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-10-07 20:50:43 +0200

    deleted

--- src/main/java/com/lessons/java/spring/ticket/platform/repo/OperatorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lessons.java.spring.ticket.platform.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lessons.java.spring.ticket.platform.model.Operator;
-
-public interface OperatorRepository extends JpaRepository<Operator, Integer> {
-
-}
