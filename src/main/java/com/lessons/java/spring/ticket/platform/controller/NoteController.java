0cb3529257246653bc4d50c5519de6189309113e
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-10-08 16:07:01 +0200

    update: added method to delete

--- src/main/java/com/lessons/java/spring/ticket/platform/controller/NoteController.java
+++ src/main/java/com/lessons/java/spring/ticket/platform/controller/NoteController.java
@@ -1,5 +1,6 @@
 package com.lessons.java.spring.ticket.platform.controller;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -34,8 +36,11 @@ public class NoteController {
 	public String store(@Valid @ModelAttribute("note") Note formNote, BindingResult bindingResult,
 			RedirectAttributes attributes, Model model) {
 
-		if (bindingResult.hasErrors())
+		if (bindingResult.hasErrors()) {
+			//Inserisco i dati nel model anche nella store perchè se si riaggiona la pagina, i campi vengono sbiancati
+			model.addAttribute("currentDate", LocalDate.now());
 			return "/notes/form-create-note";
+		}
 
 		// Aggiunto alla nota il tempo e la data di creazione
 		formNote.setCreateAt(LocalDateTime.now());
@@ -46,5 +51,21 @@ public class NoteController {
 
 		return "redirect:/tickets/show/" + formNote.getTicket().getId();
 
+	}
+	
+	/**
+	 * Elimina la nota selezionata;
+	 * @return la lista delle note aggiornate;
+	 */
+	@PostMapping("/delete/{id}")
+	public String delete(@PathVariable("id") int id) {
+
+		//Salvo il valore della nota prima che venga eliminata per poi fare la redirect corretta
+		int idTicket = service.getById(id).getTicket().getId();
+		
+		// Elimino i dati dal repository;
+		service.delete(id);
+
+		return "redirect:/tickets/show/" + idTicket + "/list-notes-ticket";
 	}
 }
