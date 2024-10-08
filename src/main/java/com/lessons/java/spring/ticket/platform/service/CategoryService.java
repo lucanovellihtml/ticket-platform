bf46f132e00ab38c51698821a0740ff554113fb9
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-10-08 12:01:39 +0200

    update: added method to return all categories from repository

--- src/main/java/com/lessons/java/spring/ticket/platform/service/CategoryService.java
+++ src/main/java/com/lessons/java/spring/ticket/platform/service/CategoryService.java
@@ -1,5 +1,26 @@
 package com.lessons.java.spring.ticket.platform.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lessons.java.spring.ticket.platform.model.Category;
+import com.lessons.java.spring.ticket.platform.repo.CategoryRepository;
+
+@Service
 public class CategoryService {
 	
+	@Autowired
+	CategoryRepository repository;
+	
+	/**
+	 * 
+	 * @return la lista delle Categories presenti nel repository
+	 */
+	public List<Category> findAllCategories() {
+
+		return repository.findAll();
+
+	}
 }
