52c7fd0750eb0d404f86ab9943e586675b9b6ba6
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-10-12 10:51:16 +0200

    update: updated api

--- src/main/java/com/lessons/java/spring/ticket/platform/controller/api/TicketRestController.java
+++ src/main/java/com/lessons/java/spring/ticket/platform/controller/api/TicketRestController.java
@@ -3,6 +3,8 @@ package com.lessons.java.spring.ticket.platform.controller.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,68 +25,81 @@ public class TicketRestController {
 
 	@Autowired
 	private TicketService service;
-	
+
 //	@Autowired
 //	private CategoryService serviceCategory;
-	
-	
 
 	/**
 	 * Metodo che restituisce tutti i ticket presenti nel repository
 	 * 
-	 * @return la lista dei tickets presenti nel repository
+	 * @return la Lista dei tickets presenti nel repository
 	 */
-	@GetMapping("list-tickets")
-	public List<Ticket> index() {
+	@GetMapping("List-tickets")
+	public ResponseEntity<List<Ticket>> index() {
 
 		List<Ticket> listTickets;
 
 		listTickets = service.findAllTickets();
 
-		return listTickets;
+		if (!listTickets.isEmpty())
+			// Controllo se la lista non è vuote
+			// Se super la condizione, restituisco un codice HTTP 200 con i dati
+			return new ResponseEntity<List<Ticket>>(listTickets, HttpStatus.OK);
+
+		// Se non entra nell'if, di default viene restitutito un codice HTTP 404
+		return new ResponseEntity<List<Ticket>>(HttpStatus.NOT_FOUND);
 
 	}
 
 	/**
-	 * Metodo che restituisce tutti i ticket presenti nel repository con il filtro status
+	 * Metodo che restituisce tutti i ticket presenti nel repository con il filtro
+	 * status
 	 * 
-	 * @return la lista dei tickets presenti nel repository con il filtro status
+	 * @return la Lista dei tickets presenti nel repository con il filtro status
 	 */
 	@GetMapping("status-filter")
-	public List<Ticket> indexStatus(@RequestParam(name = "status", required = true) String status) {
+	public ResponseEntity<List<Ticket>> indexStatus(@RequestParam(name = "status", required = true) String status) {
 
 		List<Ticket> listTickets;
-		
-		if(status!= null && !status.isEmpty()) {
+
+		if (status != null && !status.isEmpty()) {
 			listTickets = service.findAllByStatus(status);
-		}
-		else {
-			listTickets = service.findAllTickets();
+
+			// Controllo se la lista non è vuote
+			// Se super la condizione, restituisco un codice HTTP 200 con i dati
+			if (!listTickets.isEmpty())
+				return new ResponseEntity<List<Ticket>>(listTickets, HttpStatus.OK);
 		}
 
-		return listTickets;
+		// Se non entra nell'if, di default viene restitutito un codice HTTP 404
+		return new ResponseEntity<List<Ticket>>(HttpStatus.NOT_FOUND);
 
 	}
-	
+
 	/**
-	 * Metodo che restituisce tutti i ticket presenti nel repository con il filtro category
+	 * Metodo che restituisce tutti i ticket presenti nel repository con il filtro
+	 * category
 	 * 
-	 * @return la lista dei tickets presenti nel repository con il filtro category
+	 * @return la Lista dei tickets presenti nel repository con il filtro category
 	 */
 	@GetMapping("category-filter")
-	public List<Ticket> indexCategory(@RequestParam(name = "category", required = true) String category) {
+	public ResponseEntity<List<Ticket>> indexCategory(
+			@RequestParam(name = "category", required = true) String category) {
 
 		List<Ticket> listTickets;
-		
-		if(category!= null && !category.isEmpty()) {
+
+		if (category != null && !category.isEmpty()) {
 			listTickets = service.findAllByCategory(category);
+
+			// Controllo se la lista non è vuote
+			// Se super la condinzione, restituisco un codice HTTP 200 con i dati
+			if (!listTickets.isEmpty())
+				return new ResponseEntity<List<Ticket>>(listTickets, HttpStatus.OK);
 		}
-		else {
-			listTickets = service.findAllTickets();
-		}
-		
-		return listTickets;
+
+		// Se non entra nell'if, di default viene restitutito un codice HTTP 404
+		return new ResponseEntity<List<Ticket>>(HttpStatus.NOT_FOUND);
 
 	}
-	
+
 }
