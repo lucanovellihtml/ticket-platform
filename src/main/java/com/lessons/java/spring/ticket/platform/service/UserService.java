6bac2fe8e708e3f8fb20bd319c6fd2aa61032c60
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-10-14 12:46:32 +0200

    add: added method create and findAllUsersOperators

--- src/main/java/com/lessons/java/spring/ticket/platform/service/UserService.java
+++ src/main/java/com/lessons/java/spring/ticket/platform/service/UserService.java
@@ -1,5 +1,6 @@
 package com.lessons.java.spring.ticket.platform.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,11 +27,30 @@ public class UserService {
 	
 	/**
 	 * 
-	 * @return la lista dei Users presenti nel repository con lo status a false(quindi sono attivi per prendere il ticket)
+	 * @return la lista dei Users presenti nel repository con lo status a false(quindi sono gli operatos attivi per prendere il ticket)
 	 */
 	public List<User> findAllUsersStatusFalse(Boolean status) {
 		
 		return repository.findByStatus(status);
+
+	}
+	
+	/**
+	 * 
+	 * @return la lista dei Users presenti nel repository con lo status non a null(quindi gli operators)
+	 */
+	public List<User> findAllUsersOperators(Boolean statusTrue, Boolean statusFalse) {
+		
+		List<User> listUserActive, listUserNotActive; 
+		List<User> listUser = new ArrayList<User>();
+		
+		listUserActive = repository.findByStatus(statusFalse);
+		listUserNotActive = repository.findByStatus(statusTrue);
+		
+		listUser.addAll(listUserActive);
+		listUser.addAll(listUserNotActive);
+		
+		return listUser;
 
 	}
 
@@ -52,6 +72,26 @@ public class UserService {
 	public User update(User User) {
 
 		return repository.save(User);
+
+	}
+	
+	/**
+	 * @param User, l'oggetto User che deve essere create
+	 * @return il repository aggiornato
+	 */
+	public User create(User User) {
+
+		return repository.save(User);
+
+	}
+	
+	/**
+	 * @param id, id dello user da cancellare
+	 * @return il repository aggiornato
+	 */
+	public void delete(int id) {
+
+		repository.deleteById(id);
 
 	}
 }
