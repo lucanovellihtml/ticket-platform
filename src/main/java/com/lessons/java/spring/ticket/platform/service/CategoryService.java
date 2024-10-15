dc914758ac2f472c463ae858b9114e70770e0eee
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-10-15 15:37:52 +0200

    add: added method to get category by id, create,update and delete category

--- src/main/java/com/lessons/java/spring/ticket/platform/service/CategoryService.java
+++ src/main/java/com/lessons/java/spring/ticket/platform/service/CategoryService.java
@@ -10,10 +10,10 @@ import com.lessons.java.spring.ticket.platform.repo.CategoryRepository;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	CategoryRepository repository;
-	
+
 	/**
 	 * 
 	 * @return la lista delle Categories presenti nel repository
@@ -21,6 +21,47 @@ public class CategoryService {
 	public List<Category> findAllCategories() {
 
 		return repository.findAll();
+
+	}
+
+	/**
+	 * 
+	 * @param id, id della category
+	 * @return tutte le informazioni della category specifica
+	 */
+	public Category getById(int id) {
+
+		return repository.findById(id).get();
+
+	}
+
+	/**
+	 * @param Category, l'oggetto Category che deve essere aggiornato
+	 * @return il repository aggiornato
+	 */
+	public Category update(Category category) {
+
+		return repository.save(category);
+
+	}
+	
+	/**
+	 * @param Category, l'oggetto Category che deve essere creato
+	 * @return il repository aggiornato
+	 */
+	public Category create(Category category) {
+
+		return repository.save(category);
+
+	}
+	
+	/**
+	 * @param id, id della category da cancellare
+	 * @return il repository aggiornato
+	 */
+	public void delete(int id) {
+
+		repository.deleteById(id);
 
 	}
 }
