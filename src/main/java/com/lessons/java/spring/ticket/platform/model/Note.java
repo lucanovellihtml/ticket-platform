abc1c5afbff0320a33a805400ac5aee67cdd5ade
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-10-15 19:22:54 +0200

    Update Note.java

--- src/main/java/com/lessons/java/spring/ticket/platform/model/Note.java
+++ src/main/java/com/lessons/java/spring/ticket/platform/model/Note.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,6 +24,7 @@ public class Note {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
+	@Column(unique=true)
 	@NotNull(message="The description of category cannot be null!")
 	@Size(min = 2, max = 255, message="Description must have at least 2 charachters and a maximum of 255")
 	private String description;
